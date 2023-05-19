f1f5e2ea4afff9897116485890b411d365143c3f
Fingadumbledore <53864355+Fingadumbledore@users.noreply.github.com>
2023-05-19 12:46:09 +0200

    Update mate.cpp

--- mate.cpp
+++ mate.cpp
@@ -1,4 +1,4 @@
-// created by @fingadumbledore & @hyperbel
+// created by @fingadumbledore
 // Eine Mateflasche = 500 Ml
 // Das Projekt ist nur zum Spaß und Sollte nicht Ernst genommen werden
 // Der Konsum von zu viel Zucker und Koffein ist nicht Gesund und kann Gesundheitliche Schäden bewirken
@@ -7,7 +7,8 @@
 using namespace std;
 int main(){
     int  mate, Koffeingehalt, marke, kmenge, ergebnis;
-    string input, markenname;
+    string input;
+    string markenname;
 
     cout << "  **     **     **                   **                        \n";
     cout << "  **     **     **        ++         **                        \n";
@@ -29,7 +30,7 @@ int main(){
             cout << "Für einen erfolgreichen Tag benötigst du mehr als 1 Flasche Mate";
             return 0;
         }
-        cout << "Marke [1] Mio Mio Mate [2] Club Mate [3] Buenos Mate [4] Mate Mate [5] Flora Mate [6] 1337 Mate [7] Charitea Mate [8] Bionade Mate\n"
+        cout << "Marke [1] Mio Mio Mate [2] Club Mate [3] Buenos Mate [4] Mate Mate [5] Flora Mate [6] 1337 Mate [7] Charitea [8] Robin Schulz X My Mate \n"
         << "input: ";
         cin >> marke;
             mate = mate +1;
@@ -50,13 +51,14 @@ int main(){
                     Koffeingehalt = mate * 145;
                     break;
                 case 7:
-                    Koffeingehalt = mate * 15;
+                    Koffeingehalt = mate * 0;
                     break;
                 case 8:
-                    Koffeingehalt = mate * 20;
+                    Koffeingehalt = mate * 72;
                     break;
+
                 default:
-                    cout << "Dies ist keine gute Sorte, bitte reiche \neine pull request auf github ein\n";
+                    cout << "Dies ist keine gute Sorte\n";
                     break;
             }
 
@@ -75,7 +77,7 @@ int main(){
         cout << "Bitte gebe an wie viel Koffein du heute zu dir nehmen willst (in mg).\ninput: ";
         cin >> kmenge;
         cout << "Folgende Marken stehen zu verfügung\n";
-        cout << "Marke [1] Mio Mio Mate [2] Club Mate [3] Buenos Mate [4] Mate Mate [5] Flora Mate [6] 1337 Mate [7] Charitea Mate [8] Bionade Mate\ninput:";
+        cout << "[1] Mio Mio\n[2] Club Mate\n[3] Flora Mate\n[4] Mate Mate\n[5] Buenos Mate\n[6] 1337 Mate\n[7] Charitea\n\n[8] Robin Schulz X My Mate\ninput: ";
         cin >> marke;
         switch (marke)
         {
@@ -104,23 +106,27 @@ int main(){
             markenname = "1337 Mate";
             break;
         case 7: 
-            Koffeingehalt = 15;
-            markenname = "Charitea Mate";
+            Koffeingehalt = 0;
+            markenname = "Charitea";
             break;
-        case 8:
-            Koffeingehalt = 20;
-            markenname = "Bionade Mate";
+        case 8: 
+            Koffeingehalt = 72;
+            markenname = "Robin Schulz X My Mate";
             break;
+
         default:
             break;
         }
         ergebnis = kmenge / Koffeingehalt;
-        cout << "Für deinen Tag benötigst du " << ergebnis << " Flaschen " << markenname <<"\n"
-        <<"--------------------------------[Hinweis]--------------------------------\n"
-        <<"Bitte Beachte: Erhöhter Koffeingehalt. Für Kinder und schwangere oder stillende Frauen nicht empfolen\n\n";
+        cout << "Für deinen Tag benötigst du " << ergebnis << " Flaschen " << markenname ;
+        cout <<"\n";
+        cout <<"--------------------------------[Hinweis]--------------------------------\n";
+        cout <<"Bitte Beachte: Erhöhter Koffeingehalt. Für Kinder und schwangere oder stillende Frauen nicht empfolen\n";
+        cout <<"\n";
+
     }
-    
     else 
         cout << "Error";
+
 }
 
